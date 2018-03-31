3412bed646ae140719762f678cacf8da9ed642ef
Laires Pereira <lairespsoares@gmail.com>
2018-03-30 21:08:08 -0300

    criando interface de anuncio

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 $(document).ready(function(){
-    console.log('carregou')
-    $('.login-btn').click(function() { verificaLogin() })
+    //~Laires~ Verifica se o user está logado assim que a pagina carrega
+    if(window.localStorage.getItem('logado') === 'true'){
+        $('.more-options').append
+        ('<button onclick="anunciarProd()" class="dropdown-item anunciar">Anunciar</button>')//cria o botao de anunciar
+    } 
+
+    function anunciarProd(){
+            console.log('okay okay')
+    }
 })
 
-function verificaLogin(){
-    
-}
+
+
+
