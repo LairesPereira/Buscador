f35d29f41ed76c6c770e9e2d6b42d38d604c900a
Laires Pereira <lairespsoares@gmail.com>
2018-03-30 22:13:32 -0300

    interface de anuncios ok

--- js/main.js
+++ js/main.js
@@ -1,76 +1,11 @@
 $(document).ready(function(){
     //~Laires~ Verifica se o user está logado assim que a pagina carrega
     if(window.localStorage.getItem('logado') === 'true'){
-        $('.more-options').append
-        ('<button onclick="anunciarProd()" class="dropdown-item anunciar">Anunciar</button>')//cria o botao de anunciar
-    } 
-
-    function anunciarProd(){
-            console.log('okay okay')
-    }
+        $('.anunciar').css('display', 'initial')
+        //mostra o botao de anunciar
+    }    
 })
 
-<<<<<<< HEAD
-
-
-
-=======
-function verificaLogin(){
-    
+function anunciarProd(){
+    console.log('okay okay')
 }
-$(document).ready(function() {
-	$('#meuform').validate({
-		rules: {
-			Email: {
-				required: true,
-				email: true
-			},
-			password: {
-				required: true,
-				minlength: 4
-			},
-			confirm_password: {
-				required: true,
-				equalTo: "#password"
-			},
-			Nome: {
-				required: true,
-				minlength: 5
-			},
-			CPF: {
-				
-			},
-			Nascimento: {
-				
-			},
-			Numero: {
-				
-			},
-			RG: {
-				
-			},
-			Rua: {
-				
-			},
-			Bairro: {
-				
-			},
-			CEP: {
-				
-			},
-			Estado: {
-				
-			},
-			Cidade: {
-				
-			},
-			Contrato: {
-				required: true
-			}
-		},
-		messages: {
-			
-		}
-	});
-});
->>>>>>> d3a12f70af5f5b98e12ecdc7bb6a7750f2a2d605
